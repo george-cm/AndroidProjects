af302f301d0f9f77a71e81ca126780a5ca39848e
George Murga <george.murga+gitwork@gmail.com>
2015-07-06 14:28:57 +0300

    Show date under button

--- BladderDiary/app/src/main/java/com/georgemurga/bladderdiary/MyActivity.java
+++ BladderDiary/app/src/main/java/com/georgemurga/bladderdiary/MyActivity.java
@@ -1,13 +1,23 @@
 package com.georgemurga.bladderdiary;
 
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -44,10 +54,29 @@ public class MyActivity extends ActionBarActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // Intent intent = new Intent(this, DisplayMessageActivity.class);
         /*EditText editText = (EditText) findViewById(R.id.edit_message);
+
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);*/
-        startActivity(intent);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(path, "bladder_diary.csv");
+        try {
+            // Make sure the downloads directory exists
+            path.mkdirs();
+            OutputStream fos = new FileOutputStream(file, true);
+            String currentDateAndTime = sdf.format(Calendar.getInstance().getTime())
+                    + ";1" + System.getProperty("line.separator");
+            fos.write(currentDateAndTime.getBytes());
+            fos.close();
+            textView.setTextSize(20);
+            textView.setText(currentDateAndTime);
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing" + file, e);
+        }
+//        startActivity(intent);
     }
 }
